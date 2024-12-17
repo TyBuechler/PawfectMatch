fd32f27dabc05d3689689098dae18010cd777ee9
Lance Lisondra <lancelisondra9@gmail.com>
2024-12-17 07:56:45 -0600

    Lance Database

---
+++ Migrations/20241217135156_mssql.local_migration_109.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawfectMatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_109 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Adoption",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Adoption",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HasPetsBefore",
+                table: "Adoption",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HomeCheck",
+                table: "Adoption",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Adoption",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Adoption",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Preferences",
+                table: "Adoption",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Adoption");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Adoption");
+
+            migrationBuilder.DropColumn(
+                name: "HasPetsBefore",
+                table: "Adoption");
+
+            migrationBuilder.DropColumn(
+                name: "HomeCheck",
+                table: "Adoption");
+
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Adoption");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Adoption");
+
+            migrationBuilder.DropColumn(
+                name: "Preferences",
+                table: "Adoption");
+        }
+    }
+}
